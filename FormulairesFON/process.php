10f27ad72ddd7d50ff1eefeeb19e3ce836705e84
Akvmas <xakumas@gmail.com>
2024-02-09 16:40:13 +0100

    V1.2 eausec

--- FormulairesFON/process.php
+++ FormulairesFON/process.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../config.php';
 require('../vendor/autoload.php');
-ini_set('memory_limit', '256M');
+ini_set('memory_limit', '500M');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
